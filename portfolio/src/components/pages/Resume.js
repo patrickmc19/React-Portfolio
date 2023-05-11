a50c9fc57e35a06b79a852d911bfa92d584dceb1
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-11 15:21:29 -0700

    completed resume page

--- portfolio/src/components/pages/Resume.js
+++ portfolio/src/components/pages/Resume.js
@@ -2,12 +2,50 @@ import React from 'react';
 
 export default function Resume() {
   return (
-    <div>
-      <h1>My Resume</h1>
-      <p>
-        To do: link resume and add file.
-        To do: list coding proficiencies.
-      </p>
-    </div>
+    <section>
+      <div className="center">
+        <h2>My Resume</h2>
+      </div>
+      <div className="bottom-spacing">
+        <a href={require("../../assets/files/Patrick McKnight Resume.pdf")} download>
+          Click here to download my resume.
+        </a>
+      </div>
+      <div>
+        <h3>Front-end Proficiencies</h3>
+        <ol>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+          <li>Handlebars</li>
+          <li>Responsive Design</li>
+        </ol>
+        <br></br>
+        <h3>Back-end Proficiencies</h3>
+        <ol>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MySQL & Sequelize</li>
+          <li>MongoDB & Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ol>
+        <br></br>
+        <h3>Dev Tools</h3>
+        <ol>
+          <li>Git</li>
+          <li>npm</li>
+          <li>Jest</li>
+          <li>Webpack</li>
+          <li>Chrome DevTools</li>
+        </ol>
+        <br></br>
+        <h4>...and much more to come!</h4>
+      </div>
+    </section>
   );
 }
