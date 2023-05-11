4a4851d53d49987298445e97370b1064349912f0
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-10 18:25:04 -0700

    create about and contact pages. created validate email helper. renamed and reorganized pages, updated portfolioContainer, and NavTabs.

--- portfolio/src/components/pages/About.js
+++ portfolio/src/components/pages/About.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <section>
+      <div className="center" id="about">
+        <h1 className="header">About Me</h1>
+      </div>
+      <div className="center">
+        <img src={require("../../assets/images/profile-img.jpg")} alt="picture of myself" className="profile-img"/>
+      </div>
+      <div>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        To do: write about myself, skills, and approach.
       </p>
-    </div>
+      <p>
+        To do: describe musical chairs and other relevant coding experiences in the bootcamp.
+      </p>
+      </div>
+    </section>
   );
 }
--- portfolio/src/components/pages/Contact.js
+++ portfolio/src/components/pages/Contact.js
@@ -1,22 +1,97 @@
-import React from 'react';
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
 export default function Contact() {
+  const [contactState, setContactState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = contactState;
+  const [errorMessage, setErrorMessage] = useState("");
+
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const checkValid = validateEmail(e.target.value);
+      console.log(checkValid);
+      // isValid conditional statement
+      if (!checkValid) {
+        setErrorMessage("Invalid email address, try again.");
+      } else {
+        setErrorMessage("");
+      }
+      // !isValid conditional statement
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setContactState({ ...contactState, [e.target.name]: e.target.value });
+    }
+  }
+
+
+  function handleBlank(e) {
+    if (e.target.name === "Name" || e.target.name === "Message") {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setContactState({ ...contactState, [e.target.name]: e.target.value });
+    }
+  }
+
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
+    <section>
+      <div className="center">
+        <h1 className="header">Contact Me</h1>
+      </div>
+      <div>
+        <form id="contact">
+          <div>
+            <label htmlFor="name">Name:</label>
+            <br></br>
+            <input
+              type="text"
+              name="Name"
+              defaultValue={name}
+              onBlur={handleBlank}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <br></br>
+            <input
+              type="email"
+              name="email"
+              defaultValue={email}
+              onBlur={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <br></br>
+            <textarea
+              name="Message"
+              rows="6"
+              defaultValue={message}
+              onBlur={handleBlank}
+            />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <button type="submit">Send</button>
+          </form>
+      </div>
+    </section>
   );
 }
